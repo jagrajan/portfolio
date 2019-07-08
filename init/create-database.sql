2cd73a67609bf17862318c49a44c2a32e0b19571
Jagrajan Bhullar <jag@jagrajan.com>
2019-07-08 01:04:51 +0000

    Added pug rendering and placeholder template

---
+++ init/create-database.sql
@@ -0,0 +1,3 @@
+-- Database initialization script for portfolio.
+--  * Assumes a database with name 'portfolio' already exists
+
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import path from 'path';
+import pug from 'pug';
 
 const app: express.Application = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+app.use((req: express.Request, res: express.Response) => {
+  res.render('base/layout');
+});
 
 export default app;
