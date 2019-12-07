2ae221cf4db520729992881abd5c0fd674d86ce2
Jagrajan Bhullar <jag@jagrajan.com>
2019-12-07 23:44:04 +0000

    start designing homepage

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import router from './routes/index';
 
 const app: express.Application = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
